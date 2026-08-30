e358512fdf377eea8ae59187e20aa5a8fe93c73d
Egor Kozhadei <egorkozhadey@gmail.com>
2026-08-30 21:59:15 +0200

    Init Jackson example

---
+++ src/main/java/com/egor/jackson/BookApplication.java
@@ -0,0 +1,13 @@
+package com.egor.jackson;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Log
+public class BookApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookApplication.class, args);
+	}
+}
---
+++ src/test/java/com/egor/jackson/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egor.jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

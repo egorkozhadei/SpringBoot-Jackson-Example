e392f16f23ec367f064f4c6712d5ace0508ca56c
Egor Kozhadei <egorkozhadey@gmail.com>
2026-08-30 22:13:40 +0200

    JSON to Java object test

--- src/test/java/com/egor/jackson/JacksonTests.java
+++ src/test/java/com/egor/jackson/JacksonTests.java
@@ -2,15 +2,15 @@ package com.egor.jackson;
 
 import com.egor.jackson.domain.Book;
 import org.junit.jupiter.api.Test;
+import tools.jackson.core.JacksonException;
 import tools.jackson.databind.ObjectMapper;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class JacksonTests {
-
     @Test
-    public void testThatObjectMapperCanCreateJsonFromObject() {
-        ObjectMapper objectMapper = new ObjectMapper();
+    public void testThatObjectMapperCanCreateJsonFromObject() throws JacksonException {
+        final ObjectMapper objectMapper = new ObjectMapper();
         Book book = Book.builder()
                 .isbn("978-0-13-478627-5")
                 .title("The Enigma of Eternity")
@@ -22,4 +22,18 @@ public class JacksonTests {
         assertThat(result).isEqualTo("{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"yearPublished\":\"2005\"}");
     }
 
+    @Test
+    public void testThatObjectMapperCanCreateJavaObjectFromJsonObject() throws JacksonException {
+        Book book = Book.builder()
+                .isbn("978-0-13-478627-5")
+                .title("The Enigma of Eternity")
+                .author("Aria Montgomery")
+                .yearPublished("2005")
+                .build();
+        String json = "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"yearPublished\":\"2005\"}";
+        final ObjectMapper objectMapper = new ObjectMapper();
+        Book result = objectMapper.readValue(json, Book.class);
+
+        assertThat(result).isEqualTo(book);
+    }
 }

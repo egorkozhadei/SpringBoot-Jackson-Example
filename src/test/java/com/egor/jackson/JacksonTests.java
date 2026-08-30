d6eb8c8be7f4be4f900eab6227c07658b50ceffa
Egor Kozhadei <egorkozhadey@gmail.com>
2026-08-30 22:15:43 +0200

    Test JsonProperty annotation

--- src/test/java/com/egor/jackson/JacksonTests.java
+++ src/test/java/com/egor/jackson/JacksonTests.java
@@ -19,7 +19,7 @@ public class JacksonTests {
                 .build();
 
         String result = objectMapper.writeValueAsString(book);
-        assertThat(result).isEqualTo("{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"yearPublished\":\"2005\"}");
+        assertThat(result).isEqualTo("{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"year\":\"2005\"}");
     }
 
     @Test
@@ -30,10 +30,12 @@ public class JacksonTests {
                 .author("Aria Montgomery")
                 .yearPublished("2005")
                 .build();
-        String json = "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"yearPublished\":\"2005\"}";
+        String json = "{\"isbn\":\"978-0-13-478627-5\",\"title\":\"The Enigma of Eternity\",\"author\":\"Aria Montgomery\",\"year\":\"2005\"}";
         final ObjectMapper objectMapper = new ObjectMapper();
         Book result = objectMapper.readValue(json, Book.class);
 
         assertThat(result).isEqualTo(book);
     }
+
+
 }
